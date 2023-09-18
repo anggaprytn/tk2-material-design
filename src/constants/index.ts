fa1be612dee353b39e04ddbfc6ca27b87f2645ea
Angga Priyatna <anggaprytna@gmail.com>
2023-09-18 20:26:02 +0700

    chore: add constants folder

---
+++ src/constants/index.ts
@@ -0,0 +1,3 @@
+import { theme, settings, dataCard } from './data';
+
+export { theme, settings, dataCard };
--- src/screens/Home/index.tsx
+++ src/screens/Home/index.tsx
@@ -3,28 +3,14 @@ import { View, Image } from 'react-native';
 import { Pressable } from '@/components';
 import { styles } from './styles';
 import React from 'react';
-
-const data = [
-  {
-    id: 1,
-    image: 'https://picsum.photos/700',
-  },
-  {
-    id: 2,
-    image: 'https://picsum.photos/701',
-  },
-  {
-    id: 3,
-    image: 'https://picsum.photos/702',
-  },
-];
+import { dataCard } from '@/constants';
 
 const Home = () => {
   return (
     <View style={styles.container}>
       <StatusBar style="auto" />
       <View>
-        {data.map(item => (
+        {dataCard.map(item => (
           <Pressable key={item.id} style={styles.card}>
             <Image source={{ uri: item.image }} style={styles.image} />
           </Pressable>
